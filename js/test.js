96ab7622353e6f2974b9b4d58f98912128ae5722
sowoo <sowoo0412@interpark.com>
2021-10-29 16:32:16 +0900

    asdasd

--- js/test.js
+++ js/test.js
@@ -1,17 +1,17 @@
-var data = {
-    "start": {
-        "count": "5",
-        "title": "start",
-        "priorities": [{
-            "txt": "Work"
-        }, {
-            "txt": "Time Sense"
-        }, {
-            "txt": "Dicipline"
-        }, {
-            "txt": "Confidence"
-        }, {
-            "txt": "CrossFunctional"
-        }]
+data = [{
+    "Employees": {
+
+        "name": "John",
+        "age": 30,
+        "cars": ["Ford", "BMW", "Fiat"],
+
+        "name": "Jane",
+        "age": 34,
+        "cars": ["Ford", "BMW", "Fiat"],
+
+        "name": "Grogu",
+        "age": 50,
+        "cars": ["Ford", "BMW", "Fiat"]
+
     }
-}
+}];
